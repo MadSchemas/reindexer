ca99f52d786ffdad10dd8cb70391f89d7238b4e1
madschemas <155993105+MadSchemas@users.noreply.github.com>
2024-02-16 17:58:35 +0300

    fix build

--- cpp_src/cluster/logger.cc
+++ cpp_src/cluster/logger.cc
@@ -1,10 +1,12 @@
 #include "cluster/logger.h"
 #include "tools/logger.h"
 
+#include <iostream>
+
 namespace reindexer {
 namespace cluster {
 
-void Logger::print(LogLevel l, std::string& str) const { str.append("\n"); std::cout << str; } ///logPrint(l, &str[0]); }
+void Logger::print(LogLevel , std::string& str) const { str.append("\n"); std::cout << str; } ///logPrint(l, &str[0]); }
 
 }  // namespace cluster
 }  // namespace reindexer
